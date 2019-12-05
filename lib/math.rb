973fb787e87a5c1e0ab55ceb20d1cbfea8f38ddb
Damely Tineo <damely.tineo07@gmail.com>
2019-12-05 04:33:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+minus = num1 - num2
+return minus
 end
 
 def division(num1, num2)
-
+divide = num1 / num2
+return divide
 end
 
 def multiplication(num1, num2)
-
+multiply = num1 * num2
+return multiply
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+return mod
 end
 
 def square_root(num)
-
+sr = Math.sqrt
+return sr
 end
